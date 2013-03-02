5f08b6b92de89177b7a487b2996859ff61977a96
Jim Bailey <dgym.bailey@gmail.com>
2013-03-02 22:19:07 +0700

    Added Python version of pbkdf2_hmac_sha512.
    It is used if ctypes fails to load libssl.so.

--- pbkdf2_key_handler.py
+++ pbkdf2_key_handler.py
@@ -1,26 +1,61 @@
 from base64 import b64encode, b64decode
+import hmac
 import hashlib
-import ctypes
+from struct import Struct
 
 from Crypto import Random
 
 
-SSL = ctypes.cdll.LoadLibrary('libssl.so')
+try:
+    import ctypes
+    SSL = ctypes.cdll.LoadLibrary('libssl.so')
+except ImportError:
+    SSL = False
+except OSError:
+    SSL = False
 
 
-def pbkdf2_hmac_sha512(key, salt, iterations, keylen):
-    SSL.EVP_sha512.restype = ctypes.c_void_p
-    evp = ctypes.cast(SSL.EVP_sha512(), ctypes.POINTER(ctypes.c_void_p))
-    result = ctypes.create_string_buffer('', size=keylen)
-    SSL.PKCS5_PBKDF2_HMAC(
-        key, len(key),
-        salt, len(salt),
-        int(iterations),
-        evp,
-        keylen,
-        ctypes.byref(result)
-    )
-    return result.raw
+if SSL:
+    # pylint: disable=E1103
+    def pbkdf2_hmac_sha512(key, salt, iterations, keylen):
+        SSL.EVP_sha512.restype = ctypes.c_void_p
+        evp = ctypes.cast(SSL.EVP_sha512(), ctypes.POINTER(ctypes.c_void_p))
+        result = ctypes.create_string_buffer('', size=keylen)
+        SSL.PKCS5_PBKDF2_HMAC(
+            key, len(key),
+            salt, len(salt),
+            int(iterations),
+            evp,
+            keylen,
+            ctypes.byref(result)
+        )
+        return result.raw
+else:
+    # pylint: disable=E1101
+    def pbkdf2_hmac_sha512(key, salt, iterations, keylen):
+        mac = hmac.new(key, None, hashlib.sha512)
+        inner = mac.inner
+        outer = mac.outer
+        
+        def pseudorandom(data):
+            i = inner.copy()
+            i.update(data)
+            o = outer.copy()
+            o.update(i.digest())
+            return o.digest()
+        
+        pack_int = Struct('>I').pack
+
+        result = ''
+        for block in xrange(1, -(-keylen//mac.digest_size)+1):
+            u = pseudorandom(salt+pack_int(block))
+            rv = bytearray(u)
+            for _ in xrange(iterations-1):
+                u = pseudorandom(u)
+                for j in xrange(len(rv)):
+                    rv[j] ^= ord(u[j])
+            result += str(rv)
+        return result[:keylen]
 
 
 class Pbkdf2KeyHandler(object):
